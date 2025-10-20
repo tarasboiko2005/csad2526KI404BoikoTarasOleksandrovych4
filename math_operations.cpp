b32a5fe62e999465515ae91362e3b4d5f89d2f2a
tarasboiko2005 <tarasboiko2005@gmail.com>
2025-10-21 00:55:31 +0300

    Added Google Test framework and unit tests for add()

---
+++ math_operations.cpp
@@ -0,0 +1,11 @@
+
+// math_operations.cpp
+// Implementation of basic math operations declared in math_operations.h
+
+#include "math_operations.h"
+
+// Simple addition implementation
+int add(int a, int b)
+{
+    return a + b;
+}
---
+++ math_operations.h
@@ -0,0 +1,8 @@
+
+// math_operations.h
+// Small header declaring basic math operations.
+
+#pragma once
+
+// Adds two integers and returns the result.
+int add(int a, int b);
